d5360c4f3a989309f7bd681506ee4653bb8e323b
Lior Hadashian <liorhadashian@gmail.com>
2025-05-27 17:22:56 +0300

    Forgot 2

---
+++ TiltanMultiplayerFusion2/Assets/Scripts/ReadyManager.cs
@@ -0,0 +1,26 @@
+using System;
+using Fusion;
+using UnityEngine;
+
+public class ReadyManager : NetworkBehaviour
+{
+    public event Action onReadyCounterReachedMax;
+    public int readyCounter = 0;
+    
+    [Rpc]
+    public void SetReadyRPC(RpcInfo info = default)
+    {
+        Debug.Log($"Player id {info.Source.PlayerId} is ready");
+        readyCounter++;
+        if(readyCounter >= 2)
+            onReadyCounterReachedMax?.Invoke();
+    }
+
+    public override void Spawned()
+    {
+        base.Spawned();
+        LobbyManager.Instance.readyManagerInstance = this;
+        onReadyCounterReachedMax += LobbyManager.Instance.MaxPlayersReady;
+    }
+    
+}
